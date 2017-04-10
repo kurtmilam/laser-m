818e14b90cd91050e5a226340275070e0577200d
Kurt Milam <kurt.milam@xioup.com>
2017-04-10 18:09:33 +0200

    Changed UserShow to work with root state

--- src/components/User/UserShow.js
+++ src/components/User/UserShow.js
@@ -11,25 +11,21 @@ import * as X from '../../utils/xioup.main.utils'
 import M from 'User/UserModel'
 
 const UserShow =
-
   { oninit: vn =>
       { // console.log( vn )
         const id = Number( vn.attrs.id )
         const state = M.state
         const rowL = M.getRowL( id )
         const dataL = [ rowL, 'data' ]
-        // const atom = X.lensedAtom( M.getRowL( id ), M.state )
         vn.state = { id
                    , state
-                   // , atom
                    , rowL
                    , dataL
                    }
       }
   , view: vn =>
     <div>
-      { M.listRowLabel( X.view( M.dataL, M.state ) ) }
-      { /* M.listRowLabel( vn.state.atom() ) */ }
+      { M.listRowLabel( X.view( vn.state.rowL )( vn.state.state ) ) }
     </div>
   }
 
